1ac0fb108d8daa6233f7aed366721919e6efc406
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-07-02 11:19:43 -0300

    Padrão de projeto Repository para posts

---
+++ src/repositories/post/json-post-repository.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'path/win32';
+import { PostModel } from '../../models/post/post-model';
+import { PostRepository } from './post-repository';
+import { readFile } from 'fs/promises';
+
+const ROOT_DIR = process.cwd();
+const JSON_POST_FILE_PATH =   resolve(ROOT_DIR, 'src', 'db', 'seed', 'posts.json');
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisk() {
+    const jsonContent = await readFile(JSON_POST_FILE_PATH, 'utf-8');
+    const parsedJson = JSON.parse(jsonContent);
+    const {posts} = parsedJson;
+    return posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+    if (!post) {
+      throw new Error(`Post com ID ${id} não encontrado.`);
+    }
+    return post;
+  }
+}
+
+
+
