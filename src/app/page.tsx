796aa8230afefdca66898bd016a2cbfa280ac4da
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-07-26 16:07:21 -0300

    Sobre layout.tsx (root) e Metadata do Next para title e description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
 import { Suspense } from 'react';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import { Header } from '@/components/Header';
-import { Container } from '@/components/Container';
 import { PostFeatured } from '@/components/PostFeatured';
 
 // page.tsx (server) -> menu.tsx (server) -> link (client)
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
-      <PostFeatured />
+    <>
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <p className='text-6xl font-bold text-center py-8'>Footer</p>
-      </footer>
-    </Container>
+    </>
   );
 }
