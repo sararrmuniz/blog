a344583625d3424225603087dff0afc1d30fe0a7
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-07-26 15:42:29 -0300

    Usando cache

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,14 +1,13 @@
-import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
 export async function PostsList() {
-  const posts = await postRepository.findAll();
-
+  const posts = await findAllPublicPosts();
   return (
     <div>
       <div className='grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3'>
-        {posts.map(post => {
+        {posts.slice(1).map(post => {
           const postLink = `/post/${post.slug}`;
 
           return (
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -23,17 +23,17 @@ export class JsonPostRepository implements PostRepository {
     return posts;
   }
 
-  async findAll(): Promise<PostModel[]> {
+  async findAllPublic(): Promise<PostModel[]> {
     await this.simulateWait();
 
     const posts = await this.readFromDisk();
-    return posts;
+    return posts.filter((post: PostModel) => post.published);
   }
 
   async findById(id: string): Promise<PostModel> {
     await this.simulateWait()
 
-    const posts = await this.findAll();
+    const posts = await this.findAllPublic();
     const post = posts.find(post => post.id === id);
     if (!post) {
       throw new Error(`Post com ID ${id} não encontrado.`);
